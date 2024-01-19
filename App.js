48bd7d43cc652c4df841ea000a867e00de54eee6
Paweł Sroka <P.sroka@onet.eu>
2024-01-19 01:51:21 +0100

    After merge conflict cleanup

--- App.js
+++ App.js
@@ -12,15 +12,16 @@ import Profile from './component/userProfile/UserProfile.js';
 import initializeDatabase from './component/db/dbInit';
 import SearchFilter from './component/prodCatalog/SearchFilter.js';
 import ProductScreen from './component/prodCatalog/ProductScreen.js';
-import AuthViewScreen from './component/userHandling/quickAuth.js';
-import WishlistTemp from './component/mainViewComponent/wishlistTemp.js';
 import PaymentMethodConfig from './component/userProfile/PaymentMethodConfig.js';
 import AddressConfig from './component/userProfile/AdressConfig.js';
 import PhotoConfig from './component/userProfile/PhotoConfig.js';
 import CartScreen from './component/orderComponent/CartScreen.js';
 import CheckoutScreen from './component/orderComponent/Checkout.js';
 import OrderFinalized from './component/orderComponent/OrderFinalized.js';
-import { UserProvider } from './component/userHandling/UserContext.js';
+import WelcomeScreen from './component/authentication/WelcomeScreen';
+import LoginScreen from './component/authentication/LoginScreen';
+import RegisterScreen from './component/authentication/RegisterScreen';
+import AppWrapper from './AppWraper.js';
 import { CartProvider } from './component/orderComponent/CartContext.js';
 
 
@@ -66,104 +67,12 @@ function HomeStackScreen() {
   );
 }
 
-export function AuthView() {
-  return(
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="SignIn" component={AuthViewScreen}/>
-    </Stack.Navigator>
-  )
-}
-
-export function MainView() {
-  return(
-    <Tab.Navigator screenOptions={{ headerShown: false }} tabBarOptions={{ labelStyle: styles.tabText, }}>
-      <Tab.Screen name="Home" component={HomeStackScreen} options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('./assets/img/Home/Home.png')}
-            />
-          ),
-          tabBarLabel: 'Home',
-        }}/>
-      <Tab.Screen name="WishList" component={WishlistTemp} options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('./assets/img/Home/Wishlist.png')}
-            />
-          ),
-          tabBarLabel: 'Wishlist',
-        }} />
-      <Tab.Screen name="Profile" component={ProfileStackScreen} options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('./assets/img/Home/Profile.png')}
-            />
-          ),
-          tabBarLabel: 'Profile',
-        }}/>
-      <Tab.Screen name="Search" component={ProductCatalogStackScreen} options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('./assets/img/Home/Search.png')}
-            />
-          ),
-          tabBarLabel: 'Search',
-        }}/>
-    </Tab.Navigator>
-  );
-}
-
-export default function App() {
-  const [userToken, setUserToken] = React.useState('X5929755');
-
-  useEffect(() => {
-    initializeDatabase();
-    LogBox.ignoreLogs(['ViewPropTypes will be removed']);
-  }, []);
-
-import React, { useEffect, useState  } from 'react';
-import { StyleSheet, Image, LogBox, View, Text   } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import { useUser } from './component/userHandling/UserContext.js';
-
-import { FontProvider } from './component/mainViewComponent/FontContext';
-
-import Home from './component/mainViewComponent/Home.js';
-import Profile from './component/userProfile/UserProfile.js';
-import initializeDatabase from './component/db/dbInit';
-import SearchFilter from './component/prodCatalog/SearchFilter.js';
-import ProductScreen from './component/prodCatalog/ProductScreen.js';
-import WelcomeScreen from './component/authentication/WelcomeScreen';
-import LoginScreen from './component/authentication/LoginScreen';
-import RegisterScreen from './component/authentication/RegisterScreen';
-import AppWrapper from './AppWraper.js';
-
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-const HomeStack = createStackNavigator();
-const CatalogStack = createStackNavigator();
-
-
-
 function WishListPlaceholder() {
   return(
     <View>
         <Text>Wishlist</Text>
     </View>
   )    
-}
-
-function ProductCatalogStackScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>ten tekst trzeba wyrzucic</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
 }
 
 export function AuthView() {
@@ -195,7 +104,7 @@ export function MainView() {
           ),
           tabBarLabel: 'Wishlist',
         }} />
-      <Tab.Screen name="Profile" component={Profile} options={{
+      <Tab.Screen name="Profile" component={ProfileStackScreen} options={{
           tabBarIcon: () => (
             <Image
               source={require('./assets/img/Home/Profile.png')}
@@ -216,39 +125,33 @@ export function MainView() {
 }
 
 function App() {
-  const [userToken, setUserToken] = React.useState(null);
-
-  const { user } = useUser();
-  console.log('Current user:', user);
-  //const fontsLoaded = useFontsLoaded();
 
   useEffect(() => {
     initializeDatabase();
     LogBox.ignoreLogs(['ViewPropTypes will be removed']);
   }, []);
 
-  // if (!fontsLoaded) {
-  //   console.log('Fonts are loading in App.js ...');
-  //   return null;
-  // }
+  const { user } = useUser();
+  console.log('Current user:', user);
+
   return (
     
     <FontProvider> 
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {user == null ? (
-          // No token found, user isn't signed in
-          <Stack.Screen name="Auth" component={AuthView} />
-        ) : (
-          // User is signed in
-          <Stack.Screen name="MainView" component={MainView}/>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+      <CartProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {user == null ? (
+              // No token found, user isn't signed in
+              <Stack.Screen name="Auth" component={AuthView} />
+            ) : (
+              // User is signed in
+              <Stack.Screen name="MainView" component={MainView}/>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </CartProvider>
     </FontProvider>
-    
   );
-  
 }
 export default AppWrapper(App);  
 
@@ -266,11 +169,4 @@ const styles = StyleSheet.create({
     fontSize: 10,
     color: '#1F2223',
   }
-  tabText: {
-    //fontFamily: 'InterRegular',
-    fontSize: 10,
-    color: '#1F2223',
-  }
 });
-
-
